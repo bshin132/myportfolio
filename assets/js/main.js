2209c009e062164e479fb197b5f26118e6b7c88c
bshin132 <bshin132@gmail.com>
2022-06-07 16:59:13 -0700

    navbar apps now working

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//MENU SHOW Y HIDDEN
+// const navMenu = document.getElementById("nav-menu"),
+//   navToggle = document.getElementById("nav-toggle"),
+//   navClose = document.getElementById("nav-close");
+
+// //MENU SHOW
+// //VALIDATE OF CONSTANT EXISTS
+// if (navToggle) {
+//   navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show-menu");
+//   });
+// }
