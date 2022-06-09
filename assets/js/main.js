cb00c4267285a96f201d83182c9a89caeba3ac38
bshin132 <bshin132@gmail.com>
2022-06-08 21:49:16 -0700

    implement dark theme and fix styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ if (navClose) {
   });
 }
 
-//REMOVE MENU MOBILE
+//------ REMOVE MENU MOBILE ------//
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
@@ -29,7 +29,7 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-// SHOW HIDE SKILLS
+//------ SHOW HIDE SKILLS ------//
 const skillsContent = document.getElementsByClassName("skills__content"),
   skillsHeader = document.querySelectorAll(".skills__header");
 
@@ -48,7 +48,7 @@ skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
 
-// QUALIFICATION TABS
+//------ QUALIFICATION TABS ------//
 const tabs = document.querySelectorAll("[data-target]"),
   tabContents = document.querySelectorAll("[data-content]");
 
@@ -69,7 +69,7 @@ tabs.forEach((tab) => {
   });
 });
 
-//SERVICES MODAL
+//------ SERVICES MODAL ------//
 const modalViews = document.querySelectorAll(".services__modal"),
   modalBtns = document.querySelectorAll(".services__button"),
   modalCloses = document.querySelectorAll(".services__modal-close");
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
   });
 });
 
-// PORTFOLIO SWIPER
+//------ PORTFOLIO SWIPER ------//
 let swiper = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
@@ -106,7 +106,7 @@ let swiper = new Swiper(".portfolio__container", {
   },
 });
 
-// SECTIONS ACTIVE LINK
+//-------- SECTIONS ACTIVE LINK --------//
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -126,7 +126,7 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-// CHANGE BACKGROUND HEADER
+//------ CHANGE BACKGROUND HEADER ------//
 function scrollHeader(){
   const nav = document.getElementById('header')
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
@@ -134,10 +134,40 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-// SHOW SCROLL UP
+//------ SHOW SCROLL UP ------//
 function scrollUp(){
   const scrollUp = document.getElementById('scroll-up');
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+//------ DARK LIGHT THEME --------//
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
